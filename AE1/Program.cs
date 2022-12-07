e8595f4429161b5b45fffdcff1c133a62d893258
liyougan <786735912@qq.com>
2022-12-07 10:23:13 +0800

    update 流入提升升降机流程

--- AE1/Program.cs
+++ AE1/Program.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AE1.工站;
+using AE1.流程;
 
 namespace AE1 {
     class Program {
         static void Main(string[] args) {
+            var 流入提升升降机 = new 流入提升升降机();
+            var flow = new 流入提升升降机Flow(流入提升升降机);
+            flow.Run();
+            Console.WriteLine("===启动===");
+
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
         }
     }
 }
--- AE1/流程/流入提升升降机Flow.cs
+++ AE1/流程/流入提升升降机Flow.cs
@@ -23,7 +23,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using AE1.工站;
 
@@ -35,18 +37,24 @@ namespace AE1.流程 {
 
         public 流入提升升降机Flow(流入提升升降机 station) {
             this.流入提升升降机 = station;
+            Init();
         }
 
         /*=============================================[-- 参数 --]=============================================*/
         public 流入提升升降机 流入提升升降机 { get; set; }
-
+        private Step _currentStep = Step.Idle;
+        private Dictionary<Step, MethodInfo> _stepDict = null;
         public enum Step {
             Idle,
+            判断流入许可,
             流入,
+            判断流入到位,
             扫码,
             等待取料,
             下降,
+            等待流出许可,
             流出,
+            等待流出完成,
             上升,
             完成,
 
@@ -58,13 +66,71 @@ namespace AE1.流程 {
 
 
         /*=============================================[-- AutoFlow --]=============================================*/
+        public void Run() {
+            _currentStep = Step.判断流入到位;
+            var t = new Task(() => {
+                while (true) {
+                    _stepDict[_currentStep]?.Invoke(this, null);
+                    Thread.Sleep(1000);
+                }
+            }, TaskCreationOptions.LongRunning);
+            t.Start();
+        }
+
+        #region AutoFlow
+        public void 判断流入许可() {
+            Console.WriteLine("判断流入许可--OK");
+        }
+
+        public void 流入() {
+            Console.WriteLine("流入电机正转");
+        }
+
+        public void 判断流入到位() {
+            Console.WriteLine("判断流入到位--OK");
+        }
+        public void 扫码() {
+            Console.WriteLine("扫码");
+        }
+        public void 等待取料() {
+            Console.WriteLine("等待取料");
+        }
+        public void 下降() {
+            Console.WriteLine("下降");
+        }
+        public void 等待流出许可() {
+            Console.WriteLine("等待流出许可--OK");
+        }
+        public void 流出() {
+            Console.WriteLine("流出");
+        }
+        public void 等待流出完成() {
+            Console.WriteLine("等待流出完成");
+        }
+        public void 上升() {
+            Console.WriteLine("上升");
+        }
+        public void 完成() {
+            Console.WriteLine("完成");
+        }
+        #endregion
 
 
 
 
 
 
-
+        /*=============================================[-- 辅助 --]=============================================*/
+        private void Init() {
+            _stepDict = new Dictionary<Step, MethodInfo>();
+            var methods = this.GetType().GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            foreach (MethodInfo methodInfo in methods) {
+                if (Enum.GetNames(typeof(Step)).Contains(methodInfo.Name)) {
+                    var step = (Step)Enum.Parse(typeof(Step), methodInfo.Name);
+                    _stepDict.Add(step, methodInfo);
+                }
+            }
+        }
 
 
 
